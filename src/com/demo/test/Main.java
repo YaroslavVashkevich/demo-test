1aa977dfb28531290d01221081a774061575b6f4
Yaroslav Vashkevich <v-yaroslav@yandex.ru>
2022-11-06 15:50:25 +0300

    change main

--- src/com/demo/test/Main.java
+++ src/com/demo/test/Main.java
@@ -5,10 +5,7 @@ public class Main {
         System.out.println("Hello!");
         System.out.println("Hello!");
         System.out.println("Hello!");
-        System.out.println("Hello!");
         
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
